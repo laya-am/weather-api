e86b36206565b5d5956b98b0153b799d0d7e0b75
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-05-23 10:20:32 +0200

    add setTimeout and weather message

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -4,29 +4,37 @@ export default function Weather() {
     const [weather, setWeather] = useState({});
 
     useEffect(() => {
-        async function fetchWeather() {
-            try {
-                const response = await fetch(
-                    `https://example-apis.vercel.app/api/weather`
-                );
-                const data = await response.json();
-                console.log(data);
-                setWeather({
-                    condition: data.condition,
-                    temperature: data.temperature,
-                });
-                console.log("weather: ", weather);
-            } catch (error) {
-                console.error(error);
+        const timer= setTimeout(() => {
+
+            async function fetchWeather() {
+                try {
+                    const response = await fetch(
+                        `https://example-apis.vercel.app/api/weather`
+                    );
+                    const data = await response.json();
+                    console.log(data);
+                    setWeather({
+                        condition: data.condition,
+                        temperature: data.temperature,
+                        isGoodWeather: data.isGoodWeather,
+                    });
+                    console.log("weather: ", weather);
+                } catch (error) {
+                    console.error("There was an error: ",error);
+                }
             }
-        }
-        fetchWeather();
-    }, []);
+            fetchWeather();
+            console.log("new data was fetched in timeout");
+        }, 5000)
+        return () => clearTimeout(timer);
+    }, [weather]);
 
     return (
         <>
             <p>{weather.temperature}</p>
             <p>{weather.condition}</p>
+            {weather.isGoodWeather !== undefined && (weather.isGoodWeather ? <h3>Awesome weather! Let's go out and:</h3> : <h3>Bad weather outside. But we can:</h3>)}
+            {/* {weather.isGoodWeather ? <h3>Awesome weather! Let's go out and:</h3> : <h3>Bad weather outside. But we can:</h3>} */}
         </>
     );
 }
